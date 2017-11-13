2ac77fc41dcc49c75dbdbbd0dd5d3699166b3526
gwilliams_c <christopher.gwilliams@psi.ch>
2017-11-13 15:24:40 +0100

    sqlite support for storing tokens, needs a check for expiration

--- spotify_analysis.py
+++ spotify_analysis.py
@@ -16,6 +16,18 @@ class Spotify_Handler:
         self.redirect_uri = self.sp['REDIRECT']
         self.access_token = ""
         self.base_url = self.sp['API_URL']
+        self.conn = sqlite3.connect('tokens.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS tokens (service text, token text)')
+        self.conn.commit()
+        self.cursor.execute('SELECT token FROM tokens WHERE service="spotify"')
+        self.access_token = self.cursor.fetchone()[0]
+        if not self.access_token:
+            self.oauth_authorise()
+        else: 
+            print(self.access_token[0])
+            self.get_playlists()
+
 
     def get_playlists(self):
         playlists = requests.get(self.base_url + 'me/playlists', headers={
@@ -30,7 +42,7 @@ class Spotify_Handler:
     def oauth_authorise(self):
         self.o = o.Oauth_Handler('spotify')
         self.o.server.add_endpoint('/spotify', 'Spotify', self.save_code)
-        url = s.build_url()
+        url = self.build_url()
         self.o.oauth_authorise(url)
 
     def save_code(self, args):
@@ -38,9 +50,9 @@ class Spotify_Handler:
         self.o.oauth_close()
         token_res = self.o.oauth_token(self.sp['TOKEN_URL'], {'redirect_uri':self.sp['REDIRECT'], 'code':self.token, 'grant_type':"authorization_code", 'client_id':self.config['spotify']['CLIENT_ID'], 'client_secret':self.sp['CLIENT_SECRET']}, 'POST')
         self.access_token = token_res['access_token']
+        self.conn.cursor().execute('INSERT INTO tokens VALUES(?,?)', ('spotify', self.access_token))
+        self.conn.commit()
         self.refresh_token = token_res['refresh_token']
 
 
 s = Spotify_Handler()
-s.oauth_authorise()
-# s.get_playlists()
