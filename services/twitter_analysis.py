d233bab9a12bacee3671395a75ae0090d67f7b56
encima <encima@gmail.com>
2018-04-10 14:12:24 +0200

    twitter app auth support and basic searches

--- services/twitter_analysis.py
+++ services/twitter_analysis.py
@@ -20,25 +20,60 @@ class TwitterHandler:
         self.auth = OauthHandler('twitter')
         self.access_token = self.auth.get_token()
         if not self.access_token:
-            resp = dict(urllib.parse.parse_qsl(self.get_request_token()))
-            token = resp['oauth_token']
-            url =self.tw['AUTH_URL'] + self.tw['ARGS'].format(token=token)
-            print(url)
-            self.auth.oauth_authorise(url, self.save_code)
+            self.app_auth()
+        else:
+            results = self.search_tweets('Trump')
+            tweets = []
+            for t in results['statuses']:
+                tweets.append({'text': t['text'], 'user': t['user']['screen_name'], 'date': t['created_at']})
+            f = open('data/twitter_tasks.json', 'w')
+            f.write(json.dumps(tweets))
+            f.close()
+
+    def app_auth(self):
+        key_secret = '{}:{}'.format(self.tw['CLIENT_ID'], self.tw['CLIENT_SECRET']).encode('ascii')
+        b64_encoded_key = base64.b64encode(key_secret)
+        b64_encoded_key = b64_encoded_key.decode('ascii')
+        headers = {
+            'Authorization': 'Basic {}'.format(b64_encoded_key),
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        }        
+        auth_data = {
+            'grant_type': 'client_credentials'
+        }
+        auth =requests.post('https://api.twitter.com/oauth2/token', headers=headers, data=auth_data)
+        token = auth.json()['access_token']
+        if auth.status_code == 200:
+            self.auth.conn.cursor().execute('DELETE FROM tokens WHERE service="{}"'.format(self.auth.service))
+            self.auth.conn.cursor().execute('INSERT INTO tokens VALUES(?,?)', (self.auth.service, token))
+            self.auth.conn.commit()
+        else:
+            print('Error getting token')
             
 
     def build_auth_header(self, url, method = 'POST', token = None):
         NONCE = ""
         for i in range(32):
             NONCE += chr(random.randint(97, 122))
-        params = {
-            'oauth_callback': self.tw['REDIRECT'],
-            'oauth_consumer_key': self.tw['CLIENT_ID'],
-            'oauth_nonce': NONCE,
-            'oauth_signature_method': 'HMAC-SHA1',
-            'oauth_timestamp': str(int(time.time())),
-            'oauth_version': "1.0"
-        }
+        params = {}
+        if token:
+            params = {
+                'oauth_consumer_key': self.tw['CLIENT_ID'],
+                'oauth_nonce': NONCE,
+                'oauth_signature_method': 'HMAC-SHA1',
+                'oauth_timestamp': str(int(time.time())),
+                'oauth_token': token,
+                'oauth_version': "1.0"
+            }
+        else:
+            params = {
+                'oauth_callback': self.tw['REDIRECT'],
+                'oauth_consumer_key': self.tw['CLIENT_ID'],
+                'oauth_nonce': NONCE,
+                'oauth_signature_method': 'HMAC-SHA1',
+                'oauth_timestamp': str(int(time.time())),
+                'oauth_version': "1.0"
+            }
 
         HEADER_TITLE = "Authorization"
 
@@ -51,7 +86,9 @@ class TwitterHandler:
         SIGNING_KEY = urllib.parse.quote(self.tw['CLIENT_SECRET'], '') + '&'
         if token:
             SIGNING_KEY += urllib.parse.quote(token, '')
-            params['oauth_token'] = token
+        print(BASE_STRING)
+        print('----')
+        print(SIGNING_KEY)
         params['oauth_signature'] = urllib.parse.quote(base64.standard_b64encode(hmac.new(SIGNING_KEY.encode(), BASE_STRING.encode(), sha1).digest()).decode('ascii'))
 
         HEADER = "OAuth "
@@ -83,3 +120,12 @@ class TwitterHandler:
             print('Something happened when trying to get your token')
             self.auth.oauth_close()
             return None
+
+    def search_tweets(self, term):
+        url = self.tw['API_URL'] + 'search/tweets.json'
+        print(self.access_token)
+        h = {'Authorization': 'Bearer {}'.format(self.access_token)}
+        p = {'q': term, }
+        r = requests.get(url, params = p, headers = h)
+        if r.status_code == 200:
+            return r.json()
